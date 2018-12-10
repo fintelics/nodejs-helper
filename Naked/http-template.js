323e1fbdec8b1b8e598e8bb2e67c90b04b71451c
Will Zhang <willzard.zhang@gmail.com>
2018-12-09 19:12:03 -0500

    new http template

--- Naked/http-template.js
+++ Naked/http-template.js
@@ -1,1 +1,19 @@
 //This script uses the basic http requst template
+
+const https = require('https');
+
+https.get('https://google.com',(response)=>{
+
+    let data ='';
+    response.on('data',(chunk)=>{
+        data+=chunk;
+    });
+
+    response.on('end',()=>{
+        //console.log(JSON.parse(data).explanation)
+        console.log(data)
+    });
+}).on("error",(err)=>{
+    console.log("Error"+err.message);
+
+});
